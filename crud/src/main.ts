ac188d85ff8e6b42682d910f40670ee884ec9331
dariganarmanova <narmanovadariga@gmail.com>
2024-11-04 15:22:11 +0900

    crud

--- crud/src/main.ts
+++ crud/src/main.ts
@@ -5,10 +5,10 @@ interface Crud {
 
 const input = document.getElementById("input") as HTMLInputElement;
 const button = document.getElementById("submit") as HTMLButtonElement;
-const display = document.getElementById("display") as HTMLDivElement;
+//const display = document.getElementById("display") as HTMLDivElement;
 const taskList = document.getElementById("taskList") as HTMLUListElement;
 
-const tasks: Crud[] = [];
+let tasks: Crud[] = [];
 let nextId: number = 1;
 
 function create(resource: string): Crud {
@@ -21,11 +21,20 @@ function create(resource: string): Crud {
   return newTask;
 }
 
-function update(id: number, resource: string): Crud | null {
-  const task = tasks.find((task) => task.id === id);
+function update(id: number, newResource: string): Crud | null {
+  const task = tasks.find((t) => t.id === id);
   if (task) {
-    task.resource = resource;
+    task.resource = newResource;
     return task;
+  }
+  return null;
+}
+
+function del(id: number): Crud | null {
+  const initialLength = tasks.length;
+  tasks = tasks.filter((task) => task.id !== id);
+  if (tasks.length < initialLength) {
+    return { id, resource: "deleted task" };
   }
   return null;
 }
@@ -35,10 +44,34 @@ function renderTasks() {
   tasks.forEach((task) => {
     const listItem = document.createElement("li");
     listItem.textContent = `Task id: ${task.id}, Resource is ${task.resource}`;
+    const updatedButton = document.createElement("button") as HTMLButtonElement;
+    updatedButton.textContent = "Update";
+    updatedButton.addEventListener("click", () => {
+      const newResource = prompt("Enter the new one", task.resource);
+      if (newResource) {
+        update(task.id, newResource);
+        console.log("Updated task", newResource);
+        renderTasks();
+      }
+    });
+    const deleteButton = document.createElement("button") as HTMLButtonElement;
+    deleteButton.textContent = "Delete";
+    deleteButton.addEventListener("click", () => {
+      del(task.id);
+      renderTasks();
+    });
+    listItem.appendChild(updatedButton);
+    listItem.appendChild(deleteButton);
+    taskList.appendChild(listItem);
   });
 }
 
+//for each li we append the button and each li we append to ul
+
 button.addEventListener("click", () => {
   const newInput = input.value;
-  const createdTask = create(newInput);
+  create(newInput);
+  renderTasks();
 });
+
+renderTasks();
